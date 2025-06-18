ffe835a34facfdff48e951b46df0ef5f06c8d82c
강인화 <rkddlsghk1998@gmail.com>
2025-06-19 04:24:52 +0900

    feat: 회원가입 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login.tsx';
 import './App.css';
 import MyPage from "./pages/MyPage.tsx";
 import {AuthProvider} from "./auth.tsx";
 import {RequireAuth} from "./RequireAuth.tsx";
+import Signup from "./pages/Signup.tsx";
 
 function App() {
     return (
         <AuthProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/login"  element={<Login />} />
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<Signup/>}/>
                     <Route path="/mypage" element={
-                        <RequireAuth><MyPage /></RequireAuth>
-                    } />
-                    <Route path="/"       element={<Navigate to="/mypage" />} />
+                        <RequireAuth><MyPage/></RequireAuth>
+                    }/>
+                    <Route path="/" element={<Navigate to="/mypage"/>}/>
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
